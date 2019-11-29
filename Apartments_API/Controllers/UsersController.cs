ca6c5d103fba6b0c9ed39e4acd9fb0a41a977753
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-11-29 14:56:06 +0200

    Created user's registration endpoint

--- Apartments_API/Controllers/UsersController.cs
+++ Apartments_API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using Apartments_API.DTO;
 using Apartments_API.Models;
 using Apartments_API.Repository;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -14,11 +16,15 @@ namespace Apartments_API.Controllers
     {
         private readonly ILogger<UsersController> _logger;
         private readonly IRepositoryWrapper _repository;
+        private readonly IMapper _mapper;
 
-        public UsersController(IRepositoryWrapper repositoryWrapper, ILogger<UsersController> logger)
+        public UsersController(IRepositoryWrapper repositoryWrapper,
+            ILogger<UsersController> logger,
+            IMapper mapper)
         {
             _repository = repositoryWrapper;
             _logger = logger;
+            _mapper = mapper;
         }
 
         [HttpGet("all")]
@@ -27,21 +33,30 @@ namespace Apartments_API.Controllers
             return _repository.IsNaudotojas.FindAll();
         }
 
+        /// <summary>
+        /// Saves user's data to database
+        /// </summary>
+        /// <param name="user">New user data</param>
+        /// <returns>User data if user was saved successfully</returns>
         [HttpPost("register")]
-        public ActionResult<IsNaudotojas> Register([FromBody] UserCreateDto user)
+        public ActionResult<UserDto> Register([FromBody] UserCreateDto user)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest("Invalid model");
             }
 
-            var createdUser = _repository.IsNaudotojas.Create(new IsNaudotojas(user));
-            // TODO: Add user to one of the roles table
-            switch (user.Role)
+            // Checks if user does not exists
+            var checkIfExists = _repository.IsNaudotojas
+                .FindByCondition(o => o.ElPastas.ToLower().Equals(user.ElPastas.ToLower())).Any();
+            if (checkIfExists)
             {
+                return BadRequest("User already exists");
             }
 
-            return Ok();
+            // Saves user in db
+            var createdUser = _repository.IsNaudotojas.Create(user);
+            return Ok(_mapper.Map<IsNaudotojas, UserDto>(createdUser));
         }
     }
 }
--- Apartments_API/Repository/Repositories/IsNaudotojasRepository.cs
+++ Apartments_API/Repository/Repositories/IsNaudotojasRepository.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using Apartments_API.DTO;
 using Apartments_API.Models;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 namespace Apartments_API.Repository.Repositories
@@ -26,14 +29,46 @@ namespace Apartments_API.Repository.Repositories
 
         public IEnumerable<IsNaudotojas> FindByCondition(Expression<Func<IsNaudotojas, bool>> expression)
         {
-            throw new NotImplementedException();
+            return _repository.Set<IsNaudotojas>()
+                .Where((expression))
+                .Include(o => o.Savininkas)
+                .Include(o => o.Valytojas)
+                .Include(o => o.Nuomininkas)
+                .AsNoTracking();
         }
 
-        public IsNaudotojas Create(IsNaudotojas entity)
+        public IsNaudotojas Create(UserCreateDto entity)
         {
-            var createdUser = _repository.Set<IsNaudotojas>().Add(entity).Entity;
+            var user = _repository.Mapper.Map<UserCreateDto, IsNaudotojas>(entity);
+            _repository.Set<IsNaudotojas>().Add(user);
             _repository.SaveChanges();
-            return createdUser;
+
+            // Saves in one of the roles table
+            var role = entity.Role;
+            switch (role)
+            {
+                // Tenant
+                case 0:
+                    var tenant = new Nuomininkas();
+                    tenant.IdIsNaudotojas = user.IdIsNaudotojas;
+                    _repository.Set<Nuomininkas>().Add(tenant);
+                    break;
+                // Owner
+                case 1:
+                    var owner = new Savininkas();
+                    owner.IdIsNaudotojas = user.IdIsNaudotojas;
+                    _repository.Set<Savininkas>().Add(owner);
+                    break;
+                // Worker
+                case 2:
+                    var worker = new Valytojas();
+                    worker.IdIsNaudotojas = user.IdIsNaudotojas;
+                    _repository.Set<Valytojas>().Add(worker);
+                    break;
+            }
+
+            _repository.SaveChanges();
+            return user;
         }
 
         public void Update(IsNaudotojas entity)
